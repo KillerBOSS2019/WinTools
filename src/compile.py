90b3317bf23c14fca730fae6ca04a8b5dc71dc79
IMISSED <Damienweifen@gmail.com>
2022-02-08 15:00:56 -0800

    Adding Github actions

--- src/compile.py
+++ src/compile.py
@@ -1,21 +1,25 @@
 import os
 import json
 import shutil
-import zipfile
 
-with open("./src/entry.tp") as entry:
+with open("entry.tp") as entry:
     entry = json.loads(entry.read())
 
 startcmd = entry['plugin_start_cmd'].split("%TP_PLUGIN_FOLDER%")[1].split("\\")
 filedirectory = startcmd[0]
 fileName = startcmd[1]
+    
+if os.path.exists(filedirectory):
+    os.remove(os.path.join(os.getcwd(), "WinTools"))
+else:
+    os.makedirs("temp/"+filedirectory)
 
-os.makedirs(filedirectory)
 
-for file in os.listdir('./src'):
-    if file not in ["compile.py", "utils", "requirements.txt", "build", "dist", "Main.py"]:
+for file in os.listdir("."):
+    if file not in ["compile.py", "utils", "requirements.txt", "build", "dist", "Main.py", "main.spec", "__pycache__", "temp"]:
         print("copying", file)
-        shutil.copy("./src/"+file, filedirectory)
-#shutil.move("/dist/WinTools.exe", filedirectory)
+        shutil.copy(os.path.join(os.getcwd(), file), os.path.join("temp", filedirectory))
+shutil.copy(os.path.join(os.getcwd(), r"dist\Main.exe"), "temp/"+filedirectory)
 
-shutil.make_archive("WinTools.tpp", 'zip', "WinTools/"+filedirectory)
+shutil.make_archive(base_name="WinTools", format='zip', root_dir="temp", base_dir="WinTools") 
+os.rename("WinTools.zip", "WinTools.tpp")
